8e20c8b67149b6a3d9ed2b27c42642ae2d00e2ea
boguz <luis_bento@hotmail.com>
2015-11-15 13:38:10 +0100

    Update rumuli.js
    added smoothScroll function

--- js/rumuli.js
+++ js/rumuli.js
@@ -3,14 +3,32 @@ jQuery(document).ready(function ($) {
 	"use strict";
 
   menuClicks();
+	smoothScroll();
 
 }); // end of document.ready function
 
 function menuClicks() {
 
-  $('.main-nav ul li').on( "click", function() {
-    $('.main-nav ul li').removeClass('activeMenuItem');
+  $('.main-nav ul li a').on( "click", function() {
+    $('.main-nav ul li a').removeClass('activeMenuItem');
     $(this).addClass('activeMenuItem');
   });
 
+}
+
+function smoothScroll() {
+	"use strict";
+
+	$('a[href*=#]:not([href=#])').click(function () {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
 }
