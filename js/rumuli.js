c80eb1877f1fc10398fc424bfded16ee78676b1b
boguz <luis_bento@hotmail.com>
2015-11-15 17:10:12 +0100

    Update rumuli.js
    added mobile menu function

--- js/rumuli.js
+++ js/rumuli.js
@@ -1,34 +1,74 @@
-jQuery(document).ready(function ($) {
+/*
+==========================================
+  DOCUMENT READY FUNCTION
+==========================================
+*/
+jQuery(document).ready(function($) {
 
-	"use strict";
+  "use strict";
 
   menuClicks();
-	smoothScroll();
+  mobileMenu();
+  smoothScroll();
 
 }); // end of document.ready function
 
-function menuClicks() {
+/*
+==========================================
+  FUNCTIONS
+==========================================
+*/
 
-  $('.main-nav ul li a').on( "click", function() {
+
+
+/**************** MENU CLICKS ***********************/
+function menuClicks() {
+  "use strict";
+
+  $('.main-nav ul li a').on("click", function() {
     $('.main-nav ul li a').removeClass('activeMenuItem');
     $(this).addClass('activeMenuItem');
   });
 
 }
 
-function smoothScroll() {
-	"use strict";
 
-	$('a[href*=#]:not([href=#])').click(function () {
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
+/**************** MOBILE MENU ***********************/
+
+function mobileMenu() {
+  "use strict";
+
+  $('.menu-button').on("click", function() {
+    $(this).toggleClass('clicked');
+    $('.mobile-nav').toggleClass('open-mobile-menu');
+  });
+
+  $('.mobile-nav ul li a').on("click", function() {
+    $('.mobile-nav ul li a').removeClass('activeMenuItem');
+    $(this).addClass('activeMenuItem');
+    $('.menu-button').removeClass('clicked');
+    $('.mobile-nav').removeClass('open-mobile-menu');
+  });
+
+}
+
+
+/**************** SMOOTH SCROLL ***********************/
+
+function smoothScroll() {
+  "use strict";
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 }
